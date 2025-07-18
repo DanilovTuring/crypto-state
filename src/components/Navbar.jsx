991c50472c1ea73cd9b47f11be83773086a2c7d9
DanilovTuring <danilogonzalezx77@gmail.com>
2025-07-18 02:14:48 -0500

    feat: update Navbar component styles and add navigation links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,30 @@ import React from "react";
 
 function Navbar() {
     return (
-        <nav className="bg-white dark:bg-gray-800 shadow-md">
+        <nav className="bg-[--color-primary-light] dark:bg-[--color-primary-dark] shadow-md">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between h-16">
                     {/* Logo*/}
                     <div className="flex items-center">
-                        <span className="text-xl font-semibold text-gray-900 dark:text-white" >
+                       <span className="text-xl font-semibold text-[--color-primary-dark] dark:text-white">
                             CryptoState
                         </span>
                     </div>
                     {/*Menú*/}
+                    <div className="hidden md:flex items-center space-x-6">
+                        <a href="#" className="nav-link">
+                            Inicio
+                        </a>
+                        <a href="#" className="nav-link">
+                            Mercados
+                        </a>
+                        <a href="#" className="nav-link">
+                            Portafolio
+                        </a>
+                        <a href="#" className="nav-link">
+                            Contacto
+                        </a>
+                    </div>
                 </div>
             </div>
         </nav>
