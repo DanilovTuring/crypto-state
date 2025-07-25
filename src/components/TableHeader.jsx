0e1a743f2eaf772e1150b7509e02dbaaef4f5a13
DanilovTuring <danilogonzalezx77@gmail.com>
2025-07-24 19:07:51 -0500

    feat: refactor layout and styling of TableHeader

--- src/components/TableHeader.jsx
+++ src/components/TableHeader.jsx
@@ -3,40 +3,36 @@ import React from "react";
 const TableHeader = () => {
   return (
     <div
-      className="flex items-center w-full py-3 px-6 bg-white
-    border-b border-gray-200 text-gray-500 font-semibold
-    text-base sm:text-lg"
+      className="flex w-full py-2 px-3 bg-white border-b border-gray-200 text-gray-500 font-semibold text-sm sm:text-base"
       role="row"
       aria-label="Encabezado de tabla de criptomonedas"
     >
-      {/* Columna Ranking */}
-      <div className="w-[60px] text-left">
+      {/* Columna Ranking (Fija) */}
+      <div className="w-[50px] sm:w-[60px] text-left sticky left-0 bg-white z-10">
         <span>Nº</span>
       </div>
-
-      {/* Columna Moneda */}
-      <div className="w-1/5 min-w-[200px] sm:min-w-[220px]">
+      {/* Columna Moneda (Fija) */}
+      <div className="w-[120px] sm:w-1/4 text-left sticky left-[50px] sm:left-[60px] bg-white z-10">
         <span>Moneda</span>
       </div>
-
-      {/* Columna Precio */}
-      <div className="w-1/5 min-w-[140px] text-right pl-44">
-        <span>Precio</span>
-      </div>
-
-      {/* Columna Cambio 24h */}
-      <div className="w-1/5 min-w-[250px] text-right pl-32">
-        <span>Cambio 24h</span>
-      </div>
-
-      {/* Columna Capitalización */}
-      <div className="w-1/5 min-w-[140px] text-right ml-14">
-        <span>Capitalización</span>
-      </div>
-
-      {/* Columna Acción */}
-      <div className="w-[120px] text-right mr-16">
-        <span>Acción</span>
+      {/* Columnas desplazables */}
+      <div className="flex flex-1">
+        {/* Columna Precio */}
+        <div className="w-[80px] sm:w-1/5 text-left sm:text-right">
+          <span>Precio</span>
+        </div>
+        {/* Columna Cambio 24h */}
+        <div className="w-[80px] sm:w-1/5 text-left sm:text-right">
+          <span>Cambio 24h</span>
+        </div>
+        {/* Columna Capitalización */}
+        <div className="w-[80px] sm:w-1/5 text-left sm:text-right">
+          <span>Capitalización</span>
+        </div>
+        {/* Columna Acción */}
+        <div className="w-[80px] sm:w-[120px] text-left sm:text-right">
+          <span>Acción</span>
+        </div>
       </div>
     </div>
   );
